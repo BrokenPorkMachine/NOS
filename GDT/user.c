7817f68433e726ff493d218725507223e866369d
BrokenPorkMachine <rawil286@gmail.com>
2025-07-29 11:47:05 -0300

    Create user.c

---
+++ GDT/user.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+
+void __attribute__((naked)) user_task(void) {
+    asm volatile(
+        "mov $'U', %%al\n"
+        "mov $0xB8000, %%rbx\n"
+        "mov %%al, (%%rbx)\n"
+        "int $0x80\n"       // Trap back to kernel
+        "1: hlt\n"
+        "jmp 1b\n"
+        :::"rbx", "al"
+    );
+}
