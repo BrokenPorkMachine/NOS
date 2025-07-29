765b034c603c5f4237ce6748c766aca73274f3d1
BrokenPorkMachine <rawil286@gmail.com>
2025-07-29 11:21:31 -0300

    Create interrupt.c

---
+++ IDT/interrupt.c
@@ -0,0 +1,9 @@
+#include <stdint.h>
+void isr_default_handler(uint64_t *rsp) {
+    // You could output to serial here or VGA
+    volatile char *vga = (char*)0xB8000 + 160; // Line 2
+    const char *msg = "INTERRUPT!";
+    for (int i = 0; msg[i]; ++i)
+        vga[i*2] = msg[i];
+    while (1) __asm__ volatile ("hlt");
+}
