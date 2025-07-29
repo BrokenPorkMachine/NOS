19a626d92bde89b43212c74d7314fd2469473c63
BrokenPorkMachine <rawil286@gmail.com>
2025-07-29 11:33:49 -0300

    Create isr_timer_stub.asm

---
+++ IO/isr_timer_stub.asm
@@ -0,0 +1,13 @@
+; isr_timer_stub.asm
+global isr_timer_stub
+isr_timer_stub:
+    push rbp
+    mov rbp, rsp
+    cli
+    extern isr_timer_handler
+    call isr_timer_handler
+    ; Send EOI to PIC
+    mov al, 0x20
+    out 0x20, al
+    leave
+    iretq
