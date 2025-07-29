50cb7986eaeb159873645ff8a11e22cd7c665c7a
BrokenPorkMachine <rawil286@gmail.com>
2025-07-29 17:34:42 -0300

    refactor thread subsystem

--- Task/thread.h
+++ Task/thread.h
@@ -1,17 +1,27 @@
 #pragma once
 #include <stdint.h>
 
+typedef enum {
+    THREAD_READY,
+    THREAD_RUNNING,
+    THREAD_BLOCKED
+} thread_state_t;
+
 typedef struct thread {
     uint64_t rsp;
     void (*func)(void);
-    char* stack;
+    char *stack;
     int id;
-    struct thread* next; // For round-robin
+    thread_state_t state;
+    struct thread *next; // run queue link
 } thread_t;
 
 extern thread_t *current;
 
 void threads_init(void);
+thread_t *thread_create(void (*func)(void));
+void thread_block(thread_t *t);
+void thread_unblock(thread_t *t);
 void context_switch(uint64_t *old_rsp, uint64_t new_rsp);
 void enter_user_mode(uint64_t entry, uint64_t user_stack);
 void schedule(void);
