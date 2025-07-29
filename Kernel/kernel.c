442cd8093f0e23713d4efcce61ac8028d6e83640
BrokenPorkMachine <rawil286@gmail.com>
2025-07-29 11:50:38 -0300

    Update kernel.c

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -1,9 +1,11 @@
 #include <stdint.h>
 #include "../IDT/idt.h"
 #include "../VM/paging.h"
-#i#include "../Task/thread.h"
+#include "../Task/thread.h"
+#include "../GDT/gdt.h"
 
 #define VGA_TEXT_BUF 0xB8000
+extern void user_task(void);
 
 void vga_write(const char* s) {
     volatile uint16_t* vga = (uint16_t*)VGA_TEXT_BUF;
@@ -14,6 +16,13 @@ void vga_write(const char* s) {
     }
 }
 
+// Setup a minimal handler for int $0x80 (IDT vector 0x80)
+void isr_syscall_handler(void) {
+    // Print to VGA to prove kernel is back
+    volatile char* vga = (char*)0xB8000 + 160;
+    vga[0] = 'K';
+}
+
 void kernel_main(void) {
     vga_write("Mach Microkernel: Boot OK");
     idt_install();
@@ -21,6 +30,19 @@ void kernel_main(void) {
     pit_init(100);
     paging_init();
     threads_init();
+    gdt_install();
+
+    // --- Setup IDT, paging, PIT, etc. here ---
+    // Install int 0x80 handler in IDT (pointing to isr_syscall_stub, not shown)
+
+    // Allocate a user stack
+    static uint8_t user_stack[4096];
+    uint64_t user_stack_top = (uint64_t)&user_stack[4096];
+
+    // Jump to user mode!
+    enter_user_mode((uint64_t)user_task, user_stack_top);
+
+    while (1) __asm__ volatile ("hlt");
 
     // Start first thread
     asm volatile(
@@ -31,3 +53,4 @@ void kernel_main(void) {
 
     while (1) __asm__ volatile ("hlt");
 }
+
