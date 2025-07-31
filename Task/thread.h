e380aab48830ee3f70df12851b0efbd7845aa091
BrokenPorkMachine <rawil286@gmail.com>
2025-07-31 05:53:19 -0300

    Update thread.h

--- Task/thread.h
+++ Task/thread.h
@@ -4,7 +4,8 @@
 typedef enum {
     THREAD_READY,
     THREAD_RUNNING,
-    THREAD_BLOCKED
+    THREAD_BLOCKED,
+    THREAD_EXITED
 } thread_state_t;
 
 typedef struct thread {
@@ -18,12 +19,27 @@ typedef struct thread {
 
 extern thread_t *current;
 
+// Initialize threading system and create initial threads
 void threads_init(void);
+
+// Create a new kernel thread with function entrypoint
 thread_t *thread_create(void (*func)(void));
+
+// Mark thread as blocked and reschedule
 void thread_block(thread_t *t);
+
+// Mark thread as ready (for scheduler)
 void thread_unblock(thread_t *t);
+
+// Yield CPU to next ready thread (cooperative scheduling)
 void thread_yield(void);
-void context_switch(uint64_t *old_rsp, uint64_t new_rsp);
-void enter_user_mode(uint64_t entry, uint64_t user_stack);
+
+// Run the scheduler (internal, also used by yield/block/unblock)
 void schedule(void);
+
+// Switch stack (old_rsp, new_rsp): implemented in asm, never returns here
+void context_switch(uint64_t *old_rsp, uint64_t new_rsp) __attribute__((noreturn));
+
+// Enter user mode at given entry/stack: implemented in asm
+void enter_user_mode(uint64_t entry, uint64_t user_stack) __attribute__((noreturn));
 
