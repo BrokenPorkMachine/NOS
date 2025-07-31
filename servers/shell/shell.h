10fe4b68e7972ccea0745bb9895ce2933ce68221
BrokenPorkMachine <rawil286@gmail.com>
2025-07-31 06:03:51 -0300

    Update shell.h

--- servers/shell/shell.h
+++ servers/shell/shell.h
@@ -7,5 +7,5 @@
 // Entry point for the NOS kernel shell.
 // Call this from your kernel main thread after IPC init.
 void shell_main(ipc_queue_t *q, uint32_t self_id);
-
+void *__memcpy_chk(void *dest, const void *src, size_t n, size_t destlen);
 #endif // SHELL_H
