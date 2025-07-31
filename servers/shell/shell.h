8090f528dc9398284689beb7b5b5ab10f728a8b7
BrokenPorkMachine <rawil286@gmail.com>
2025-07-31 05:34:05 -0300

    Update shell.h

--- servers/shell/shell.h
+++ servers/shell/shell.h
@@ -1,5 +1,11 @@
 #ifndef SHELL_H
 #define SHELL_H
+
+#include <stdint.h>
 #include "../../IPC/ipc.h"
+
+// Entry point for the NOS kernel shell.
+// Call this from your kernel main thread after IPC init.
 void shell_main(ipc_queue_t *q, uint32_t self_id);
-#endif
+
+#endif // SHELL_H
