8fe5bdfed96c5b1008004c32dc23ddb3fd662209
BrokenPorkMachine <rawil286@gmail.com>
2025-07-31 06:54:05 -0300

    Delete bootloader/src/elf_loader.h

--- bootloader/src/elf_loader.h
+++
@@ -1,8 +0,0 @@
-// src/elf_loader.h
-#pragma once
-
-#include "../include/efi.h"
-#include "../include/bootinfo.h"
-
-EFI_STATUS load_and_boot_kernel(EFI_FILE_PROTOCOL *kernel_file, bootinfo_t *bootinfo,
-                               EFI_BOOT_SERVICES *BS, EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut);
