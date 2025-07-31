120d7077c2127366d497f544e0738341e56dbc01
BrokenPorkMachine <rawil286@gmail.com>
2025-07-31 05:40:17 -0300

    Update server.h

--- servers/nitrfs/server.h
+++ servers/nitrfs/server.h
@@ -1,16 +1,22 @@
 #ifndef NITRFS_SERVER_H
 #define NITRFS_SERVER_H
+
 #include "../../IPC/ipc.h"
 
+// NITRFS IPC message types (match these in your shell/server code)
 enum {
-    NITRFS_MSG_CREATE = 1,
-    NITRFS_MSG_WRITE,
-    NITRFS_MSG_READ,
-    NITRFS_MSG_DELETE,
-    NITRFS_MSG_LIST,
-    NITRFS_MSG_CRC,
-    NITRFS_MSG_VERIFY
+    NITRFS_MSG_CREATE = 1,   // Create file (msg.data=name, arg1=capacity, arg2=perm)
+    NITRFS_MSG_WRITE,        // Write file  (arg1=handle, arg2=len, msg.data=buffer)
+    NITRFS_MSG_READ,         // Read file   (arg1=handle, arg2=len)
+    NITRFS_MSG_DELETE,       // Delete file (arg1=handle)
+    NITRFS_MSG_LIST,         // List files  (no args)
+    NITRFS_MSG_CRC,          // Compute CRC (arg1=handle)
+    NITRFS_MSG_VERIFY,       // Verify CRC  (arg1=handle)
+    // Add new messages above this line if needed.
+    NITRFS_MSG_RESERVED      // Not used: for forward compatibility!
 };
 
+// Server entry point: call from main thread/task.
 void nitrfs_server(ipc_queue_t *q, uint32_t self_id);
-#endif
+
+#endif // NITRFS_SERVER_H
