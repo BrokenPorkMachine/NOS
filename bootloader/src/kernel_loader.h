ec3755d8a01d577f929e603eddce5eb367caa8f2
BrokenPorkMachine <rawil286@gmail.com>
2025-07-31 07:45:13 -0300

    Update kernel_loader.h

--- bootloader/src/kernel_loader.h
+++ bootloader/src/kernel_loader.h
@@ -3,7 +3,7 @@
 #include "../include/bootinfo.h"
 
 EFI_STATUS load_and_boot_kernel(
-    EFI_FILE_PROTOCOL *KernelFile,
+    struct EFI_FILE_PROTOCOL *KernelFile,
     struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut,
     EFI_BOOT_SERVICES *BS,
     bootinfo_t *bootinfo);
