0c87b4d9f3d20a18fab61d4f91a820e707e5959a
BrokenPorkMachine <rawil286@gmail.com>
2025-07-31 06:06:37 -0300

    Update libc.h

--- src/libc.h
+++ src/libc.h
@@ -13,3 +13,6 @@ int strncmp(const char *s1, const char *s2, size_t n);
 void *malloc(size_t size);
 void *calloc(size_t nmemb, size_t size);
 void free(void *ptr);
+void *__memcpy_chk(void *dest, const void *src, size_t n, size_t destlen);
+char *__strncpy_chk(char *dest, const char *src, size_t n, size_t destlen);
+
