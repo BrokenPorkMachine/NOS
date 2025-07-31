3887c9186655addc7314ccc5f92816e97636f5ff
BrokenPorkMachine <rawil286@gmail.com>
2025-07-31 06:59:15 -0300

    Update kernel_loader.h

--- bootloader/src/kernel_loader.h
+++ bootloader/src/kernel_loader.h
@@ -2,9 +2,8 @@
 #include "../include/efi.h"
 #include "../include/bootinfo.h"
 
-// Loads a kernel ELF file (already-opened), prints info, and jumps to entry.
-// Returns 0 on success, error code otherwise.
 EFI_STATUS load_and_boot_kernel(
     EFI_FILE_PROTOCOL *KernelFile,
     EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut,
+    EFI_BOOT_SERVICES *BS,
     bootinfo_t *bootinfo);
