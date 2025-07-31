bc57664ec8733584a4870d1b4c0da33f9df69bcc
BrokenPorkMachine <rawil286@gmail.com>
2025-07-30 23:55:09 -0300

    Update efi.c

--- bootloader/include/efi.c
+++ bootloader/include/efi.c
@@ -1,6 +1,7 @@
+// efi.c
 #include "efi.h"
 
-// Print a Unicode string to UEFI console
+// Print a Unicode string to the UEFI console
 EFI_STATUS efi_print(EFI_SYSTEM_TABLE *SystemTable, CHAR16 *msg) {
     if (!SystemTable || !SystemTable->ConOut || !msg) {
         return EFI_LOAD_ERROR;
@@ -23,7 +24,7 @@ EFI_STATUS efi_allocate_pages(EFI_SYSTEM_TABLE *SystemTable, UINT32 allocation_t
     );
 }
 
-// Get UEFI memory map
+// Get the UEFI memory map
 EFI_STATUS efi_get_memory_map(EFI_SYSTEM_TABLE *SystemTable, UINTN *memory_map_size, EFI_MEMORY_DESCRIPTOR *memory_map, UINTN *map_key, UINTN *descriptor_size, UINT32 *descriptor_version) {
     if (!SystemTable || !SystemTable->BootServices) {
         return EFI_LOAD_ERROR;
@@ -48,4 +49,42 @@ EFI_STATUS efi_exit_boot_services(EFI_SYSTEM_TABLE *SystemTable, EFI_HANDLE Imag
         ImageHandle,
         map_key
     );
+}
+
+// Open the root volume of the filesystem on which the EFI application is located
+EFI_STATUS efi_open_root_volume(EFI_SYSTEM_TABLE *SystemTable, EFI_HANDLE ImageHandle, EFI_FILE_PROTOCOL **Root) {
+    EFI_STATUS status;
+    EFI_BOOT_SERVICES *BS = SystemTable->BootServices;
+    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *fs;
+
+    status = BS->HandleProtocol(
+        ImageHandle,
+        &gEfiSimpleFileSystemProtocolGuid,
+        (void**)&fs
+    );
+
+    if (status != EFI_SUCCESS) return status;
+
+    return fs->OpenVolume(fs, Root);
+}
+
+// Open a file from EFI filesystem
+EFI_STATUS efi_open_file(EFI_FILE_PROTOCOL *Root, CHAR16 *FileName, EFI_FILE_PROTOCOL **File) {
+    return Root->Open(
+        Root,
+        File,
+        FileName,
+        EFI_FILE_MODE_READ,
+        0
+    );
+}
+
+// Read data from an opened EFI file
+EFI_STATUS efi_read_file(EFI_FILE_PROTOCOL *File, UINTN *BufferSize, VOID *Buffer) {
+    return File->Read(File, BufferSize, Buffer);
+}
+
+// Close an opened EFI file
+EFI_STATUS efi_close_file(EFI_FILE_PROTOCOL *File) {
+    return File->Close(File);
 }
