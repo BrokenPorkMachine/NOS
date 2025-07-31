1bd5de35287fa6a88b440c31340b587ec593ccd3
BrokenPorkMachine <rawil286@gmail.com>
2025-07-31 05:51:35 -0300

    Update user_mode.asm

--- Task/user_mode.asm
+++ Task/user_mode.asm
@@ -1,22 +1,37 @@
 global enter_user_mode
 
-; RDI - entry point
-; RSI - user stack pointer (top)
+; void enter_user_mode(void *entry, void *user_stack)
+; Arguments:
+;   rdi = entry point (RIP)
+;   rsi = user stack pointer (top, for RSP)
+;
+; Uses GDT selectors:
+;   0x23 = user data (ring 3)
+;   0x1B = user code (ring 3)
 
+section .text
 enter_user_mode:
-    mov rcx, rdi        ; entry address
-    mov rax, rsi        ; user stack
+    ; Set up segment selectors for user mode
+    mov  ax, 0x23         ; user data selector | 3
+    mov  ds, ax
+    mov  es, ax
+    mov  fs, ax
+    mov  gs, ax
+    mov  ss, ax
 
-    mov ax, 0x23        ; user data selector | 3
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
+    ; Prepare iretq stack frame (SS, RSP, RFLAGS, CS, RIP)
+    mov  rcx, rdi         ; entry point -> rcx
+    mov  rax, rsi         ; user stack  -> rax
 
-    push 0x23           ; SS
-    push rax            ; RSP
-    pushfq
-    push 0x1B           ; CS (user code selector | 3)
-    push rcx            ; RIP
-    iretq
+    push 0x23             ; SS (user data)
+    push rax              ; RSP (user stack pointer)
+    pushfq                ; RFLAGS (preserve IF)
+    push 0x1B             ; CS (user code)
+    push rcx              ; RIP (entry point)
+
+    iretq                 ; Enter user mode!
+
+    ; Should never return
+    hlt
+    jmp $
+
