5f97ee1a1d90d803d221f55a04c4d1c080cfbae7
BrokenPorkMachine <rawil286@gmail.com>
2025-07-31 04:53:20 -0300

    Update bootinfo.h

--- bootloader/include/bootinfo.h
+++ bootloader/include/bootinfo.h
@@ -2,9 +2,8 @@
 #define BOOTINFO_H
 
 #include <stdint.h>
-
-#define BOOTINFO_MAGIC_UEFI      0x55454649  // 'UEFI'
-#define BOOTINFO_MAGIC_MB2       0x36d76289  // Multiboot2 standard magic
+#define BOOTINFO_MAGIC_UEFI 0x55454649   // 'UEFI'
+#define BOOTINFO_MAGIC_MB2  0x36d76289
 
 typedef struct bootinfo_memory {
     uint64_t addr;
@@ -14,19 +13,15 @@ typedef struct bootinfo_memory {
 } bootinfo_memory_t;
 
 typedef struct bootinfo {
-    uint32_t magic;             // BOOTINFO_MAGIC_UEFI or BOOTINFO_MAGIC_MB2
-    uint32_t size;              // sizeof(struct bootinfo)
-    uint64_t kernel_phys_base;  // (optional) where kernel is loaded
-    uint64_t kernel_phys_end;   // (optional) kernel end
-    uint32_t cpu_count;         // (optional, if SMP)
-    uint32_t reserved;          // align
-    // --- memory map ---
+    uint32_t magic;
+    uint32_t size;
+    uint64_t kernel_phys_base;
+    uint64_t kernel_phys_end;
+    uint32_t cpu_count;
+    uint32_t reserved;
     bootinfo_memory_t* mmap;
     uint32_t mmap_entries;
-    // --- modules, framebuffers, etc, can go here
-    // char bootloader[32];   // optional
-    // void* framebuffer;     // optional
-    // etc
+    // Extend as needed!
 } bootinfo_t;
 
 #endif
