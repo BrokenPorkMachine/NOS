ef4bedb9624ca0ed501e91e40a71b547096ca3f3
BrokenPorkMachine <rawil286@gmail.com>
2025-07-31 07:45:30 -0300

    Update kernel_loader.h

--- bootloader/src/kernel_loader.h
+++ bootloader/src/kernel_loader.h
@@ -5,5 +5,5 @@
 EFI_STATUS load_and_boot_kernel(
     struct EFI_FILE_PROTOCOL *KernelFile,
     struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut,
-    EFI_BOOT_SERVICES *BS,
+    struct EFI_BOOT_SERVICES *BS,
     bootinfo_t *bootinfo);
