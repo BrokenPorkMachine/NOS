b81cc64823657c483fe583accf3cff8e9909b8ed
BrokenPorkMachine <rawil286@gmail.com>
2025-07-31 05:50:57 -0300

    Update context_switch.asm

--- Task/context_switch.asm
+++ Task/context_switch.asm
@@ -1,18 +1,30 @@
 global context_switch
+; void context_switch(uint64_t *old_rsp, uint64_t new_rsp);
+; rdi = pointer to save old rsp
+; rsi = new rsp value
+
+section .text
 context_switch:
-    ; Arguments: rdi = &old_rsp, rsi = new_rsp
+    ; Save callee-saved registers as per System V AMD64 ABI
     push rbp
     push rbx
     push r12
     push r13
     push r14
     push r15
+
+    ; Store current stack pointer to *old_rsp
     mov [rdi], rsp
+
+    ; Switch to the new stack
     mov rsp, rsi
+
+    ; Restore callee-saved registers (from new context's stack)
     pop r15
     pop r14
     pop r13
     pop r12
     pop rbx
     pop rbp
+
     ret
