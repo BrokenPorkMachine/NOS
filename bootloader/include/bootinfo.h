d13b208bb39a1e559e69061021bc43a2a72c7d96
BrokenPorkMachine <rawil286@gmail.com>
2025-07-31 12:57:33 -0300

    Ensure bootloader/kernel bootinfo layout matches

--- bootloader/include/bootinfo.h
+++ bootloader/include/bootinfo.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <stdint.h>
+#include <stddef.h>
+
+#pragma pack(push,1)
 #define BOOTINFO_MAGIC_UEFI 0x55454649
 #define BOOTINFO_MAGIC_MB2  0x36d76289
 #define BOOTINFO_MAX_MMAP    128
@@ -36,3 +39,11 @@ typedef struct bootinfo {
     uint64_t reserved[8];
     void *kernel_entry;
 } bootinfo_t;
+
+#pragma pack(pop)
+
+_Static_assert(sizeof(bootinfo_memory_t) == 24, "bootinfo_memory_t size mismatch");
+_Static_assert(sizeof(bootinfo_framebuffer_t) == 32, "bootinfo_framebuffer_t size mismatch");
+_Static_assert(sizeof(bootinfo_cpu_t) == 16, "bootinfo_cpu_t size mismatch");
+_Static_assert(sizeof(bootinfo_t) == 648 || sizeof(bootinfo_t) == 656,
+               "bootinfo_t unexpected size");
