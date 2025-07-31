36105e764478874a5fdc634ffaa253e27c97a5aa
BrokenPorkMachine <rawil286@gmail.com>
2025-07-31 06:04:30 -0300

    Update shell.h

--- servers/shell/shell.h
+++ servers/shell/shell.h
@@ -8,4 +8,5 @@
 // Call this from your kernel main thread after IPC init.
 void shell_main(ipc_queue_t *q, uint32_t self_id);
 void *__memcpy_chk(void *dest, const void *src, size_t n, size_t destlen);
+char *__strncpy_chk(char *dest, const char *src, size_t n, size_t destlen);
 #endif // SHELL_H
