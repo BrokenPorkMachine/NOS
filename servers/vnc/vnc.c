0d42c8b1a53e590676249b8ced6a309d3f7835e1
BrokenPorkMachine <rawil286@gmail.com>
2025-08-01 09:44:54 -0300

    Add port-based loopback network stack

--- servers/vnc/vnc.c
+++ servers/vnc/vnc.c
@@ -4,22 +4,25 @@
 #include "../../Net/netstack.h"
 #include <string.h>
 
+// Port used for VNC traffic on the loopback stack
+#define VNC_PORT 1
+
 void vnc_server(ipc_queue_t *q, uint32_t self_id) {
     (void)q; (void)self_id;
     serial_puts("[vnc] VNC server starting\n");
     const char hello[] = "NOS VNC ready\r\n";
-    net_send(hello, strlen(hello));
+    net_send(VNC_PORT, hello, strlen(hello));
     char buf[64];
     for (;;) {
-        int n = net_receive(buf, sizeof(buf) - 1);
+        int n = net_receive(VNC_PORT, buf, sizeof(buf) - 1);
         if (n > 0) {
             buf[n] = '\0';
             if (!strncmp(buf, "ping", 4)) {
                 const char pong[] = "pong\r\n";
-                net_send(pong, strlen(pong));
+                net_send(VNC_PORT, pong, strlen(pong));
             } else {
                 const char unk[] = "unknown\r\n";
-                net_send(unk, strlen(unk));
+                net_send(VNC_PORT, unk, strlen(unk));
             }
         }
         thread_yield();
