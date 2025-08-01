f62662da60e986daf5793d41d5fa650ed97009b2
BrokenPorkMachine <rawil286@gmail.com>
2025-08-01 09:32:53 -0300

    Implement safety checks for memory functions and simple VNC

--- servers/vnc/vnc.c
+++ servers/vnc/vnc.c
@@ -7,8 +7,21 @@
 void vnc_server(ipc_queue_t *q, uint32_t self_id) {
     (void)q; (void)self_id;
     serial_puts("[vnc] VNC server starting\n");
-    const char msg[] = "NOS VNC not implemented\r\n";
-    net_send(msg, strlen(msg));
-    while (1)
+    const char hello[] = "NOS VNC ready\r\n";
+    net_send(hello, strlen(hello));
+    char buf[64];
+    for (;;) {
+        int n = net_receive(buf, sizeof(buf) - 1);
+        if (n > 0) {
+            buf[n] = '\0';
+            if (!strncmp(buf, "ping", 4)) {
+                const char pong[] = "pong\r\n";
+                net_send(pong, strlen(pong));
+            } else {
+                const char unk[] = "unknown\r\n";
+                net_send(unk, strlen(unk));
+            }
+        }
         thread_yield();
+    }
 }
