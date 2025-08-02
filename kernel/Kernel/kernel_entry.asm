bd17b5c95c8b03561b1c208349a4a4bea09cc90e
BrokenPorkMachine <rawil286@gmail.com>
2025-08-02 06:00:35 -0300

    Use bootloader stack and report total RAM

--- kernel/Kernel/kernel_entry.asm
+++ kernel/Kernel/kernel_entry.asm
@@ -1,9 +1,3 @@
-section .bss
-align 16
-kernel_stack:
-    resb 16384            ; 16 KiB kernel stack
-kernel_stack_top:
-
 section .text
 global _start
 
@@ -12,16 +6,8 @@ extern kernel_main
 
 _start:
     cld
-
-    ; rdi contains the bootinfo pointer from the bootloader
-    mov rax, rdi          ; preserve bootinfo pointer
-
-    ; Set up a small kernel stack
-    lea rsp, [kernel_stack_top]
+    ; Bootloader already set up stack and passed bootinfo in rdi
     xor rbp, rbp
-
-    ; Pass bootinfo pointer to kernel_main in rdi
-    mov rdi, rax
     call kernel_main
 
 .hang:
