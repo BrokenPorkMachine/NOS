8d560cac92828397853bee48560b5e05af1f8313
BrokenPorkMachine <rawil286@gmail.com>
2025-08-02 03:21:14 -0300

    Add window server with basic IPC and demos

---
+++ user/servers/window/demo1.c
@@ -0,0 +1,18 @@
+#include "demo1.h"
+#include "window.h"
+#include "../../libc/libc.h"
+#include "../../../kernel/drivers/IO/serial.h"
+
+void window_demo1(ipc_queue_t *q, uint32_t self_id, uint32_t server) {
+    int win = window_create(q, server, 20, 20, 120, 80);
+    if (win < 0)
+        return;
+    window_draw_rect(q, server, (uint32_t)win, 0, 0, 120, 80, 0xFFFF0000);
+    ipc_message_t ev;
+    while (window_get_event(q, &ev) == 0) {
+        if (ev.type == WINDOW_MSG_KEY && ev.arg1 == (uint32_t)win) {
+            char buf[2] = {(char)ev.arg2, '\0'};
+            serial_puts(buf);
+        }
+    }
+}
---
+++ user/servers/window/demo1.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <stdint.h>
+#include "../../../kernel/IPC/ipc.h"
+
+void window_demo1(ipc_queue_t *q, uint32_t self_id, uint32_t server);
