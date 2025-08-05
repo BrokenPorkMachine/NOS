fa33c074ff99afb6c22cedec5a6d7bddd54cc9e3
BrokenPorkMachine <rawil286@gmail.com>
2025-08-05 18:04:35 -0300

    Create isr.h

---
+++ kernel/arch/IDT/isr.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "context.h"
+
+// All handlers receive a pointer to context
+void isr_default_handler(struct isr_context *ctx) __attribute__((noreturn));
+void isr_timer_handler(struct isr_context *ctx);
+void isr_syscall_handler(struct isr_context *ctx);
+void isr_keyboard_handler(struct isr_context *ctx);
+void isr_mouse_handler(struct isr_context *ctx);
+void isr_page_fault_handler(struct isr_context *ctx);
+void isr_ipi_handler(struct isr_context *ctx);
