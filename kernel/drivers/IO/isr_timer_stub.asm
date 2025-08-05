0fc61c98cf76da04eaccf2c6b5b46707862c8a81
BrokenPorkMachine <rawil286@gmail.com>
2025-08-05 18:12:52 -0300

    Update isr_timer_stub.asm

--- kernel/drivers/IO/isr_timer_stub.asm
+++ kernel/drivers/IO/isr_timer_stub.asm
@@ -1,16 +1,65 @@
-; isr_timer_stub.asm
 global isr_timer_stub
+extern isr_timer_handler
+
+section .text
+
 isr_timer_stub:
-    push rbp
-    mov rbp, rsp
     cli
-    extern isr_timer_handler
+    ; Save all general-purpose registers (order must match your C struct)
+    push r15
+    push r14
+    push r13
+    push r12
+    push r11
+    push r10
+    push r9
+    push r8
+    push rdi
+    push rsi
+    push rbp
+    push rbx
+    push rdx
+    push rcx
+    push rax
+
+    ; Push interrupt vector number (for timer, usually IRQ0 = 32)
+    mov rax, 32
+    push rax
+    ; Push dummy error code (0 for IRQs)
+    xor rax, rax
+    push rax
+    ; Push dummy CR2 (not needed for timer, but keeps context struct uniform)
+    xor rax, rax
+    push rax
+
+    ; Pass context pointer (rsp) in rdi
+    mov rdi, rsp
     call isr_timer_handler
+
+    ; Pop dummy CR2, error code, int_no
+    add rsp, 24
+
+    ; Restore registers in exact reverse order
+    pop rax
+    pop rcx
+    pop rdx
+    pop rbx
+    pop rbp
+    pop rsi
+    pop rdi
+    pop r8
+    pop r9
+    pop r10
+    pop r11
+    pop r12
+    pop r13
+    pop r14
+    pop r15
+
     ; Send EOI to PIC
     mov al, 0x20
     out 0x20, al
-    leave
+
     iretq
 
-; Indicate that this object file does not require an executable stack
 section .note.GNU-stack noalloc nobits align=1
