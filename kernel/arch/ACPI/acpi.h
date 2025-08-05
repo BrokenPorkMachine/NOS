c8be204e737af9c123fe38c752d14a9c4f573d32
BrokenPorkMachine <rawil286@gmail.com>
2025-08-05 18:11:11 -0300

    Update acpi.h

--- kernel/arch/ACPI/acpi.h
+++ kernel/arch/ACPI/acpi.h
@@ -2,5 +2,9 @@
 #include <stdint.h>
 #include "../../../boot/include/bootinfo.h"
 
+// Initializes ACPI tables using boot-provided RSDP pointer.
+// Sets up DSDT and parses MADT/LAPICs for processor info.
 void acpi_init(bootinfo_t *bootinfo);
+
+// Returns a pointer to the loaded DSDT table header, or NULL if not available.
 const void *acpi_get_dsdt(void);
