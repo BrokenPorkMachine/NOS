a3b592411c1811641077e7f95f18e8ed21e15e44
BrokenPorkMachine <rawil286@gmail.com>
2025-08-06 03:55:22 -0300

    Update libc.h

--- user/libc/libc.h
+++ user/libc/libc.h
@@ -6,6 +6,13 @@
 #include <time.h>
 #include <pthread.h>
 
+typedef struct FILE FILE;
+
+FILE *fopen(const char *path, const char *mode);
+size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream);
+size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream);
+int fclose(FILE *stream);
+
 // String/mem functions
 void *memset(void *s, int c, size_t n);
 void *memcpy(void *dest, const void *src, size_t n);
