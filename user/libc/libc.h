8ce4eb8c8071fe3740b050ff107e16d966d68a8e
BrokenPorkMachine <rawil286@gmail.com>
2025-08-06 03:53:37 -0300

    Update libc.h

--- user/libc/libc.h
+++ user/libc/libc.h
@@ -6,14 +6,6 @@
 #include <time.h>
 #include <pthread.h>
 
-typedef void* pthread_mutexattr_t;
-
-int pthread_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr);
-int pthread_mutex_lock(pthread_mutex_t *mutex);
-int pthread_mutex_unlock(pthread_mutex_t *mutex);
-int pthread_mutex_destroy(pthread_mutex_t *mutex);
-
-
 // String/mem functions
 void *memset(void *s, int c, size_t n);
 void *memcpy(void *dest, const void *src, size_t n);
