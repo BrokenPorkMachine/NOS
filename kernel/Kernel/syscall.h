9b70bb512045967939812279e4ad4b99f9ade929
BrokenPorkMachine <rawil286@gmail.com>
2025-08-06 03:03:20 -0300

    Update syscall.h

--- kernel/Kernel/syscall.h
+++ kernel/Kernel/syscall.h
@@ -5,17 +5,17 @@
 extern "C" {
 #endif
 
-// Enumerate all syscall numbers used in your kernel
 enum syscall_num {
-    SYS_YIELD        = 0,
-    SYS_WRITE_VGA    = 1,
-    SYS_FORK         = 2,
-    SYS_EXEC         = 3,
-    SYS_SBRK         = 4,
-    // Future: SYS_CLOCK_GETTIME = 5, SYS_VM_ALLOCATE = 6, ...
+    SYS_YIELD         = 0,
+    SYS_WRITE_VGA     = 1,
+    SYS_FORK          = 2,
+    SYS_EXEC          = 3,
+    SYS_SBRK          = 4,
+    SYS_CLOCK_GETTIME = 5,
+    SYS_VM_ALLOCATE   = 6,
+    // Add more as needed
 };
 
-// Main syscall dispatcher (implemented in syscall.c)
 uint64_t syscall_handle(uint64_t num, uint64_t arg1, uint64_t arg2, uint64_t arg3);
 
 #ifdef __cplusplus
