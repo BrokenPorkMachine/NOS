dbc26b7948a194576df96317cc53ef1f01d7bd26
BrokenPorkMachine <rawil286@gmail.com>
2025-08-06 05:11:12 -0300

    Update bootinfo.h

--- boot/include/bootinfo.h
+++ boot/include/bootinfo.h
@@ -1,12 +1,12 @@
-#pragma once
-#include <stdint.h>
-#include <stddef.h>
+#ifndef BOOTINFO_H
+#define BOOTINFO_H
 
-#pragma pack(push,1)
-#define BOOTINFO_MAGIC_UEFI 0x55454649
-#define BOOTINFO_MAGIC_MB2  0x36d76289
-#define BOOTINFO_MAX_MMAP    128
-#define BOOTINFO_MAX_CPUS    32
+#include <stdint.h>
+#define BOOTINFO_MAGIC_UEFI 0x4E49545255454649ULL // "NITRUEFI"
+#define BOOTINFO_MAX_MMAP 128
+#define BOOTINFO_MAX_SEGS 32
+#define KERNEL_FMT_ELF64 1
+#define KERNEL_FMT_MACHO64 2
 
 typedef struct {
     uint64_t addr, len;
@@ -15,35 +15,35 @@ typedef struct {
 } bootinfo_memory_t;
 
 typedef struct {
-    uint64_t address;
-    uint32_t width, height, pitch, bpp;
-    uint32_t type;
-    uint32_t reserved;
-} bootinfo_framebuffer_t;
+    uint32_t fmt; // 1=ELF64, 2=MachO
+    uint32_t nsegs;
+    struct {
+        uint64_t vaddr;
+        uint64_t paddr;
+        uint64_t fileoff;
+        uint64_t filesz;
+        uint64_t memsz;
+        uint32_t prot;
+        uint32_t flags;
+    } seg[BOOTINFO_MAX_SEGS];
+    uint64_t file_base;   // Pointer to kernel file mapping, for on-demand paging.
+    uint64_t file_size;
+} bootinfo_kernel_segments_t;
 
 typedef struct {
-    uint32_t processor_id, apic_id, flags, reserved;
-} bootinfo_cpu_t;
-
-typedef struct bootinfo {
-    uint32_t magic, size;
-    const char *bootloader_name;
-    const char *cmdline;
+    uint64_t magic;
+    uint64_t size;
+    char*    bootloader_name;
+    char*    cmdline;
+    uint32_t cpu_count;
+    struct { uint32_t processor_id, apic_id, flags; } cpus[8];
     bootinfo_memory_t *mmap;
     uint32_t mmap_entries;
-    bootinfo_framebuffer_t *framebuffer;
     uint64_t acpi_rsdp;
-    bootinfo_cpu_t cpus[BOOTINFO_MAX_CPUS];
-    uint32_t cpu_count;
-    void *mb2_tags;
-    uint64_t reserved[8]; // reserved[0] stores total mmap entries before clamping
-    void *kernel_entry;
+    void*    framebuffer;
+    void*    kernel_entry;
+    bootinfo_kernel_segments_t kernel_segs;
+    uint8_t  reserved[256];
 } bootinfo_t;
 
-#pragma pack(pop)
-
-_Static_assert(sizeof(bootinfo_memory_t) == 24, "bootinfo_memory_t size mismatch");
-_Static_assert(sizeof(bootinfo_framebuffer_t) == 32, "bootinfo_framebuffer_t size mismatch");
-_Static_assert(sizeof(bootinfo_cpu_t) == 16, "bootinfo_cpu_t size mismatch");
-_Static_assert(sizeof(bootinfo_t) == 648 || sizeof(bootinfo_t) == 656,
-               "bootinfo_t unexpected size");
+#endif
