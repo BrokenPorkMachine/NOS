338f6c0bf9945d44d85aa5b8869262822485f278
BrokenPorkMachine <rawil286@gmail.com>
2025-08-06 03:48:55 -0300

    Update libc.c

--- user/libc/libc.c
+++ user/libc/libc.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <time.h>
+#include <pthread.h>
 
 // ================== STRING AND MEMORY ===================
 void *memset(void *s, int c, size_t n) {
