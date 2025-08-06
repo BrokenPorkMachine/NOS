8a5ecbe686ff898504684b97bbbf6afc96fddca2
BrokenPorkMachine <rawil286@gmail.com>
2025-08-06 16:07:49 -0300

    Update regx.h

--- include/regx.h
+++ include/regx.h
@@ -1,19 +1,10 @@
 #pragma once
 #include <stdint.h>
-#define REGX_MAX_ENTRIES 128
-
-// Registry entry type identifiers
-enum {
-    REGX_TYPE_DEVICE = 1,
-    REGX_TYPE_DRIVER,
-    REGX_TYPE_FILESYSTEM,
-    REGX_TYPE_AGENT,
-    REGX_TYPE_SERVICE,
-};
+#define REGX_MAX_ENTRIES 256
 
 typedef struct {
     char name[32];
-    int  type;                // e.g. 1=device, 2=driver, 3=agent
+    int  type;                // 1=device, 2=driver, 3=agent, 4=service, 5=bus
     char version[16];
     char abi[16];
     char capabilities[64];
@@ -23,12 +14,10 @@ typedef struct regx_entry {
     uint64_t id;
     uint64_t parent_id;      // 0=root
     regx_manifest_t manifest;
-    // room for extensions
 } regx_entry_t;
 
 typedef struct {
-    // Selector/filter: can be extended (e.g. by type, capability, parent, etc)
-    int type;
-    uint64_t parent_id;
-    char name_prefix[16];
+    int type;                // 0=any
+    uint64_t parent_id;      // 0=any
+    char name_prefix[16];    // ""=any
 } regx_selector_t;
