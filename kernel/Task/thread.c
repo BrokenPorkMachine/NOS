f5333cbc6c8df6d61f7131b692db15fa606fcc0e
BrokenPorkMachine <rawil286@gmail.com>
2025-08-09 04:08:36 -0300

    Update thread.c

--- kernel/Task/thread.c
+++ kernel/Task/thread.c
@@ -490,4 +490,5 @@ void threads_init(void) {
     ipc_grant(&init_queue,  init->id, IPC_CAP_SEND | IPC_CAP_RECV);
 
     // main_thread already installed in threads_early_init
+    thread_yield();
 }
