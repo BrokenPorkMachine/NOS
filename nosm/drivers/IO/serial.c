6ca864598317b0ab39ad22c141cfa363c1737ae1
BrokenPorkMachine <rawil286@gmail.com>
2025-08-10 05:15:24 -0300

    Update serial.c

--- nosm/drivers/IO/serial.c
+++ nosm/drivers/IO/serial.c
@@ -6,16 +6,6 @@
 #define COM1 0x3F8
 #define COM1_PORT 0x3F8
 
-static inline void outb(uint16_t port, uint8_t val) {
-    __asm__ volatile ("outb %0, %1" :: "a"(val), "Nd"(port));
-}
-
-static inline uint8_t inb(uint16_t port) {
-    uint8_t ret;
-    __asm__ volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
-    return ret;
-}
-
 static int serial_is_transmit_empty(void) {
     return inb(COM1_PORT + 5) & 0x20;
 }
