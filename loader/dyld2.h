906728a5ca50fba1819f12f74dc1d5e15f6335c8
BrokenPorkMachine <rawil286@gmail.com>
2025-08-12 09:16:19 -0300

    Create dyld2.h

---
+++ loader/dyld2.h
@@ -0,0 +1,21 @@
+// ============================================================================
+// loader/dyld2.h
+// ============================================================================
+#ifndef DYLD2_H
+#define DYLD2_H
+#include "../rt/agent_abi.h"
+#include <stddef.h>
+
+typedef void* mo2_handle_t;
+typedef void* mo2_sym_t;
+
+int          dyld2_init(const AgentAPI* api);
+mo2_handle_t dyld2_dlopen(const char* path, int flags); // flags: 1=EAGER, 2=NODELETE
+mo2_sym_t    dyld2_dlsym(mo2_handle_t h, const char* name);
+int          dyld2_dlclose(mo2_handle_t h);
+
+/* optional: start an executable image (transfer control) */
+typedef int (*mo2_main_entry)(int argc, const char** argv);
+int          dyld2_run_exec(const char* path, int argc, const char** argv);
+
+#endif
