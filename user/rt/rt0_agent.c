ab14c5751a56ebadd0da5ddf6fe13fe7e43d8f9b
BrokenPorkMachine <rawil286@gmail.com>
2025-08-13 10:21:40 -0300

    Update rt0_agent.c

--- user/rt/rt0_agent.c
+++ user/rt/rt0_agent.c
@@ -1,16 +1,9 @@
-// user/rt/rt0_agent.c
 #include "../rt/agent_abi.h"
+extern int main(int, const char**);
 
-const AgentAPI *NOS;
-uint32_t        NOS_TID;
-
-extern void agent_main(void);           // or init_main(api, tid)
-
-__attribute__((noreturn))
-void _start(const AgentAPI *api, uint32_t tid) {
-    __asm__ __volatile__("andq $-16, %%rsp" ::: "rsp");
-    NOS     = api;
-    NOS_TID = tid;
-    agent_main();                       // or: init_main(NOS, NOS_TID);
+void init_main(const AgentAPI *api, unsigned self_tid) {
+    (void)api;
+    (void)self_tid;
+    main(0, NULL);
     for (;;) __asm__ __volatile__("hlt");
 }
