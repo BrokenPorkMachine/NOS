7ecab9bca5aa74dd142935f327e9650e747784a5
BrokenPorkMachine <rawil286@gmail.com>
2025-08-13 05:23:47 -0300

    Update lapic.h

--- kernel/arch/APIC/lapic.h
+++ kernel/arch/APIC/lapic.h
@@ -2,5 +2,5 @@
 #include <stdint.h>
 
 void lapic_eoi(void);
-void lapic_timer_init(uint8_t vector);  // NEW
-void lapic_enable(void);                // NEW
+void lapic_enable(void);
+void lapic_timer_init(uint8_t vector);
