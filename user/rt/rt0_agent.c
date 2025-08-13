2148fd03943142547c30078fbf512b60d57eb8f6
BrokenPorkMachine <rawil286@gmail.com>
2025-08-13 10:23:56 -0300

    Delete user/rt/rt0_agent.c

--- user/rt/rt0_agent.c
+++
@@ -1,9 +0,0 @@
-#include "../rt/agent_abi.h"
-extern int main(int, const char**);
-
-void init_main(const AgentAPI *api, unsigned self_tid) {
-    (void)api;
-    (void)self_tid;
-    main(0, NULL);
-    for (;;) __asm__ __volatile__("hlt");
-}
